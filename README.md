828524b541287664917b96124b4395d9e8f546cd
Bruno Alves <bruno.alves@infos.local>
2017-04-20 15:33:41 +0100

    first commit :)

---
+++ README.md
@@ -0,0 +1,1 @@
+# nodeJSToDoList first API
---
+++ api/routes/todolistRoute.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(app){
+	var todolist = require('../controllers/todolistController');
+
+
+	//todolist Routes
+	app.route('/tasks')
+		.get(todolist.list_all_tasks)
+		.post(todolist.create_a_task);
+
+	app.route('/tasks/:taskId')
+		.get(todolist.read_a_task)
+		.put(todolist.update_a_task)
+		.delete(todolist.delete_a_task);
+
+	
+}
